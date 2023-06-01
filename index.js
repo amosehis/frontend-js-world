0650fd2e1941e5be233d360744757bbb6fca62d0
AMOS EHIS <121234066+amosehis@users.noreply.github.com>
2023-06-01 15:49:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+let a =30
+let b=20
+let c=40
+ console.log(a+b+c);
+
+ 
+
+
+ const pie =22
+ console.log(pie)
+
+
+ const wholenumber =2
+ console.log (wholenumber)
+
+ var var1Name = 10
+ var var2Name = 20
+ var var3Name = var1Name * var2Name;
+ console.log(var1Name+var2Name+var3Name)
+
+
+ const Data = 2*2
+ const Airtime = 2*3
+ console.log(Data-Airtime)
+
+
+ 
